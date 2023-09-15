da1a70398d383f21a4695be150751d0e1b4569b1
abrarsyed123 <--global>
2023-09-15 16:00:07 +0530

    initial commit

---
+++ test.tf
@@ -0,0 +1,8 @@
+provider "aws"{
+  region = "us-east-1"
+  access_key = "AKIAR4422YT7QUV7SDVH"
+  secret_key = "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"
+}
+resource "aws_s3_bucket" "test"{
+  bucket = "my-first-terraform-bucket"
+}
