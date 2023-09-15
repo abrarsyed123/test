3392f7ef8061732801adfb266cb07fe37117c6b1
abrarsyed123 <--global>
2023-09-15 20:35:37 +0530

    ec2

--- test.tf
+++ test.tf
@@ -3,8 +3,10 @@ provider "aws"{
   access_key = "AKIAR4422YT7QUV7SDVH"
   secret_key = "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"
 }
-resource "aws_instance" "test"{
+resource "aws_instance" "test" {
   instance_type = "t2.micro"
   ami = "ami-053b0d53c279acc90"
-  Name = "Terrafom"
+  tags = {
+    Name = "Terrafom"
+  }
 }
